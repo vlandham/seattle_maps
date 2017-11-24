7716ea433c804008c2a743dd6d22b2e3bb9044ff
Jim Vallandingham <vlandham@gmail.com>
2017-11-24 14:32:51 -0800

    update subtitle

--- tools/create_thumbnails.sh
+++ tools/create_thumbnails.sh
@@ -2,5 +2,6 @@
 
 input_dir="img/maps"
 output_dir="img/thumb/"
+pattern="rand_mcnally_1924.jpg"
 
-mogrify -resize 400 -quality 85 -path $output_dir $input_dir/*.jpg
+mogrify -resize 400 -quality 85 -path $output_dir $input_dir/$pattern
